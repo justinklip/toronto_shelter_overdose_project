096f6627b1981198213169461177224c7178398f
justinklip <jklip1428@gmail.com>
2024-11-25 21:55:02 -0500

    add more to cleaning script
    janitor the names

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -12,56 +12,27 @@
 library(tidyverse)
 library(dplyr)
 library(gtools)
+library(janitor)
+#### Retrieve Data ####
 
-### Retrieve the Data ### 
+occupancy_raw_data <- read_csv("data/01-raw_data/occupancy_raw_data.csv")
+overdose_raw_data <- read_csv("data/01-raw_data/overdose_raw_data.csv")
 
-occupancy_raw_data_2021 <- read_csv("data/01-raw_data_2021.csv")
-occupancy_raw_data_2022 <- read_csv("data/01-raw_data_2022.csv")
-occupancy_raw_data_2023 <- read_csv("data/01-raw_data_2023.csv")
+### Clean Data ### 
 
-#### Merge Shelter Data ####
-occupancy_data <- bind_rows(
-  occupancy_raw_data_2023,
-  occupancy_raw_data_2022,
-  occupancy_raw_data_2021
-)
+# Clean column names
+occupancy_raw_data <- occupancy_raw_data %>%
+  clean_names()
+overdose_raw_data <- overdose_raw_data %>%
+  clean_names()
 
+# Make sure the years in both data sets align (drop pre-2021 in overdose data)
+overdose_raw_data <- overdose_raw_data %>%
+  filter(year %in% c(2021, 2022, 2023))
 
-#### Clean data ####
+#
 
-
-occupancy_raw_data <- 
-overdose_raw_data <- read_csv("/data/01-raw_data.csv")
-
-### Append the Occupancy Data Together ### 
-smartbind(occupancy_raw_data_2021, occupancy_raw_data_2022, occupancy_raw_data_2023)
-
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+### Merge Data ### 
 
 #### Save data ####
 write_csv(cleaned_data, "outputs/data/analysis_data.csv")
